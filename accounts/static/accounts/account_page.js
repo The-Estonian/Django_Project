f08b13c0b606f15ae1ff0120c9975dd5a2dc2d62
The-Estonian <Zaar2213@gmail.com>
2022-06-05 11:55:13 +0300

    account page half done

---
+++ accounts/static/accounts/account_page.js
@@ -0,0 +1,74 @@
+console.log("Account page connected!");
+
+const backdrop = document.querySelector(".backdrop")
+const accountInfo = document.querySelector(".account-info")
+const cancelButton = document.querySelectorAll(".change__label__choices__cancel")
+
+const username = document.querySelector(".username-change")
+const usernameTrigger = document.querySelector("#username")
+
+const password = document.querySelector(".password-change")
+const passwordTrigger = document.querySelector("#password")
+
+const email = document.querySelector(".email-change")
+const emailTrigger = document.querySelector("#email")
+
+const firstName = document.querySelector(".first-name-change")
+const firstNameTrigger = document.querySelector("#first-name")
+
+const lastName = document.querySelector(".last-name-change")
+const lastNameTrigger = document.querySelector("#last-name")
+
+usernameTrigger.addEventListener("click", function() {
+    backdrop.style.display = "block"
+    username.style.display = "block"
+    accountInfo.style.display = "none"
+})
+
+passwordTrigger.addEventListener("click", function() {
+    backdrop.style.display = "block"
+    password.style.display = "block"
+    accountInfo.style.display = "none"
+})
+
+emailTrigger.addEventListener("click", function() {
+    backdrop.style.display = "block"
+    email.style.display = "block"
+    accountInfo.style.display = "none"
+})
+
+firstNameTrigger.addEventListener("click", function() {
+    backdrop.style.display = "block"
+    firstName.style.display = "block"
+    accountInfo.style.display = "none"
+})
+
+lastNameTrigger.addEventListener("click", function() {
+    backdrop.style.display = "block"
+    lastName.style.display = "block"
+    accountInfo.style.display = "none"
+})
+
+for (let i = 0; i < cancelButton.length; i++) {
+    cancelButton[i].addEventListener("click", function() {
+        backdrop.style.display = ""
+        username.style.display = ""
+        password.style.display = ""
+        email.style.display = ""
+        firstName.style.display = ""
+        lastName.style.display = ""
+        accountInfo.style.display = "flex"
+    })
+}
+
+
+
+backdrop.addEventListener("click", function() {
+    backdrop.style.display = ""
+    username.style.display = ""
+    password.style.display = ""
+    email.style.display = ""
+    firstName.style.display = ""
+    lastName.style.display = ""
+    accountInfo.style.display = "flex"
+})
--- accounts/views.py
+++ accounts/views.py
@@ -5,10 +5,32 @@ from django.contrib.auth.models import User
 def user_accounts(request):
     current_user_id = request.user.id
     current_user = User.objects.filter(id = current_user_id).values()
-    print(current_user)
+    # print(current_user)
     context = {"current_user":current_user}
-    return render(request, "accounts/account_page.html", context)
-
+    if request.method == "POST":
+        current_user = request.user.id
+        user_instance = User.objects.get(id = current_user)
+        new_username = request.POST.dict()["username"]
+        old_password = request.POST.dict()["old-password"]
+        new_password = request.POST.dict()["new-password"]
+        new2_password = request.POST.dict()["new2-password"]
+        email = request.POST.dict()["email"]
+        first_name = request.POST.dict()["first-name"]
+        last_name = request.POST.dict()["last-name"]
+        print("*"*20)
+        print(current_user)
+        print(user_instance)
+        print(new_username)
+        print(old_password)
+        print(new_password)
+        print(new2_password)
+        print(email)
+        print(first_name)
+        print(last_name)
+        print(request.POST)
+        return render(request, "accounts/account_page.html", context)
+    else:
+        return render(request, "accounts/account_page.html", context)
 
 """
 <QuerySet [
@@ -22,4 +44,14 @@ def user_accounts(request):
     'is_superuser': False, 
     'last_login': datetime.datetime(2022, 6, 2, 20, 7, 9, 447758, tzinfo=datetime.timezone.utc), 
     'date_joined': datetime.datetime(2022, 5, 28, 20, 16, 58, 975808, tzinfo=datetime.timezone.utc)}]>
+
+    <QueryDict: 
+    {'csrfmiddlewaretoken': ['ES2g0pJoI1tYoyjZCCU9DHwqUP44oqOcug0lyWc250XTSQob4PJ5uqNlENfGWt81'], 
+    'username': ['Arno'], 
+    'old-password': [''], 
+    'new-password': [''], 
+    'new2-password': [''], 
+    'email': [''], 
+    'first_name': [''], 
+    'last_name': ['']}>
 """
